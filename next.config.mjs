a5bfcc18de243039df6b40fae82b01940181e32c
Ameet Jayawant <ameetj85@gmail.com>
2025-05-28 05:34:39 -0400

    added CORS info to nextjs.config

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,28 @@ const nextConfig = {
       },
     ],
   },
+
+  async headers() {
+    return [
+      {
+        // Apply these headers only to routes matching /api/*
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS,PATCH",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
